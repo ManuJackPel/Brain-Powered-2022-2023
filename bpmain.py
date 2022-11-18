f94e9e7c3e3f21afb03924b4f20522f266d9b45d
scrie100 <36298464+scrie100@users.noreply.github.com>
2022-11-18 18:33:07 +0100

    De eerste versie, een leeg main.py bestand met alleen de lsl stream erin

---
+++ bpmain.py
@@ -0,0 +1,35 @@
+#-----------------------------------------------------
+#IMPORT LIBRARIES
+#-----------------------------------------------------
+from datetime import datetime
+
+import numpy as np
+
+from pylsl import StreamInlet, resolve_stream
+
+
+
+#-----------------------------------------------------
+#MAIN LOOP
+#-----------------------------------------------------
+
+#define main loop
+def main():
+        
+    # first resolve an EEG stream on the lab network
+    print("ls")
+    streams = resolve_stream('type', 'EEG')
+
+    # create a new inlet to read from the stream
+    inlet = StreamInlet(streams[0])
+
+    # main loop
+    while True:
+        # get a new sample
+        sample, timestamp = inlet.pull_sample()
+        sample = np.array(sample)
+        print(sample)    
+       
+# start main loop
+if __name__ == '__main__':
+    main()
