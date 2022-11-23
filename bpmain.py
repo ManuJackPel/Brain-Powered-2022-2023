f7dc40092d903dd3ae9b892503f91738def7a311
scrie100 <36298464+scrie100@users.noreply.github.com>
2022-11-23 18:33:36 +0100

    the script as of now saves the data to a csv and stops after 2 seconds to check how many samples per second

--- bpmain.py
+++ bpmain.py
@@ -3,10 +3,15 @@
 #-----------------------------------------------------
 
 from datetime import datetime
-
+import time
 import numpy as np
 
+# pylsl for lsl streaming, the streaming technology used by openvibe to stream to python (and other environments)
 from pylsl import StreamInlet, resolve_stream
+
+# twisteds loopingcall is used to synchronize python loops
+from twisted.internet import task
+from twisted.internet import reactor
 
 #-----------------------------------------------------
 #IMPORT CLASSES
@@ -20,7 +25,7 @@ from pylsl import StreamInlet, resolve_stream
 
 #define main loop
 def main():
-        
+
     # first resolve an EEG stream on the lab network
     print("ls")
     streams = resolve_stream('type', 'EEG')
@@ -28,13 +33,28 @@ def main():
     # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
 
+    #measure starting time
+    starttime = time.time()
+    print(starttime)
+
     # main loop
-    while True:
+    data = np.zeros(9)
+    while True: 
         # get a new sample
         sample, timestamp = inlet.pull_sample()
         sample = np.array(sample)
-        print(sample)    
+        data = np.vstack((data, sample))
+        #print(data)
+        print(sample, timestamp)
+        np.savetxt("data.csv", data, delimiter = ",")
+        
+        #check loopstate
+        if time.time()-starttime > 2:
+            print("stop")
+            break
        
 # start main loop
 if __name__ == '__main__':
-    main()
+    #main()
+    task.LoopingCall(main).start(1.0)
+    #reactor.run()
