65b65d29db6f96c52d1bb7d8e17b9afe42f9bdef
M-wag <twelveandvoosted@gmail.com>
2022-12-14 15:35:56 +0100

    _

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-#-----------------------------------------------------
-#IMPORT LIBRARIES
-#-----------------------------------------------------
-
 from datetime import datetime
-
 import numpy as np
-
 from pylsl import StreamInlet, resolve_stream
 
-#-----------------------------------------------------
-#IMPORT CLASSES
-#-----------------------------------------------------
-
-#import classes.insertclasshere
-
-#-----------------------------------------------------
-#MAIN LOOP
-#-----------------------------------------------------
-
-#define main loop
 def main():
-        
-    # first resolve an EEG stream on the lab network
+    # Resolve an EEG stream on the lab network
     print("ls")
     streams = resolve_stream('type', 'EEG')
 
     # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
+
+    # Init Classifier
 
     # main loop
     while True:
@@ -34,6 +18,9 @@ def main():
         sample, timestamp = inlet.pull_sample()
         sample = np.array(sample)
         print(sample)    
+        # Pass Data to Pipeline
+        # Send Command To Drone
+        
        
 # start main loop
 if __name__ == '__main__':
