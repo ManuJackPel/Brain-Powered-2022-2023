e8d30c608e31ca8a2190e66a5a328b830a711f0a
M-wag <twelveandvoosted@gmail.com>
2022-12-14 15:49:14 +0100

    revert main to Nov23 version

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from datetime import datetime
-<<<<<<< HEAD
-import numpy as np
-from pylsl import StreamInlet, resolve_stream
+#-----------------------------------------------------
+#IMPORT LIBRARIES
+#-----------------------------------------------------
 
-def main():
-    # Resolve an EEG stream on the lab network
-=======
+from datetime import datetime
 import time
 import numpy as np
 
@@ -16,26 +13,29 @@ from pylsl import StreamInlet, resolve_stream
 from twisted.internet import task
 from twisted.internet import reactor
 
+#-----------------------------------------------------
+#IMPORT CLASSES
+#-----------------------------------------------------
+
 #import classes.insertclasshere
+
+#-----------------------------------------------------
+#MAIN LOOP
+#-----------------------------------------------------
 
 #define main loop
 def main():
 
     # first resolve an EEG stream on the lab network
->>>>>>> 85a9b13460e218918cc4eb92b4ae07ad82106721
     print("ls")
     streams = resolve_stream('type', 'EEG')
 
     # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
 
-<<<<<<< HEAD
-    # Init Classifier
-=======
     #measure starting time
     starttime = time.time()
     print(starttime)
->>>>>>> 85a9b13460e218918cc4eb92b4ae07ad82106721
 
     # main loop
     data = np.zeros(9)
@@ -43,16 +43,6 @@ def main():
         # get a new sample
         sample, timestamp = inlet.pull_sample()
         sample = np.array(sample)
-<<<<<<< HEAD
-        print(sample)    
-        # Pass Data to Pipeline
-        # Send Command To Drone
-        
-       
-# start main loop
-if __name__ == '__main__':
-    main()
-=======
         data = np.vstack((data, sample))
         #print(data)
         print(sample, timestamp)
@@ -68,4 +58,3 @@ if __name__ == '__main__':
     #main()
     task.LoopingCall(main).start(1.0)
     #reactor.run()
->>>>>>> 85a9b13460e218918cc4eb92b4ae07ad82106721
