fa95733152cb49146eceb5c154d7bb55d9f02a18
M-wag <twelveandvoosted@gmail.com>
2023-02-23 11:43:04 +0100

    started impelementatio of parameter checker

--- _code/classes/classifier.py
+++ _code/classes/classifier.py
@@ -1,5 +1,51 @@
 from sklearn.neighbors import KNeighborsClassifier, RadiusNeighborsClassifier
 from typing import Any
+from .parameter_checker import ParameterChecker
+
+
+Part = tuple[str, Any]
+valid_classifier_parameters = {
+        'classifier' : str,
+        'n_neighbors' : int,
+        'algorithm' : str,
+        }
+ClfParCheck = ParameterChecker(valid_classifier_parameters)
+
+class Product1():
+    """
+    It makes sense to use the Builder pattern only when your products are quite
+    complex and require extensive configuration.
+
+    Unlike in other creational patterns, different concrete builders can produce
+    unrelated products. In other words, results of various builders may not
+    always follow the same interface.
+    """
+
+    def __init__(self) -> None:
+        self.parts = {}
+
+    def add(self, part: Part) -> None:
+        """Add part to builder"""
+        if not self.is_valid_part(part):
+            return 
+        part_name, part_value = part
+        self.parts[part_name] = part_value
+
+    def is_valid_part(self, part) -> bool:
+        part_name, part_value = part
+        if not ClfParCheck.is_valid_parameter_names(part_name):
+            print(f"'{part_name}' is not a valid Class parameter name")
+            return False
+        if not ClfParCheck.is_valid_parameter_types(part):
+            print(f"{part_value} has type {type(part_value)}, does not match type for '{part_name}', should be {ClfParCheck.return_paramter_type(part_name)}")
+            return False
+        return True
+
+    def list_parts(self) -> list:
+        return self.parts
+    
+    def build(self) -> None:
+        pass
 
 class Classifier():
     """Interface for accessing EEG classification methods"""
--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -10,7 +10,8 @@ targetd = pard + '/Brain-Powered-2022-2023'
 sys.path.append(pard)
 
 # from _code.classes.classifier import Classifer, method_argument_checker
-from _code.classes.classifier import Classifier, is_valid_method_arguments
+from _code.classes.classifier import Classifier, Product1, is_valid_method_arguments
+from _code.classes.errors import BuilderIsEmpty, BuilderMissingEssentialParts
 
 keyword_to_class = {
         'kNN' : 'KNeighborsClassifier()',
@@ -40,6 +41,53 @@ def test_method_argument_checker():
     assert is_valid_method_arguments('kNN', kNN_args) == True
     kNN_args['dummy_argument'] = 'Nonsense'
     assert is_valid_method_arguments('kNN', kNN_args) == False
+
+def test_clf_builder_return_params():
+    clf_builder = Product1()
+    clf_builder.add(('classifier', 'kNN'))
+    clf_builder.add(('n_neighbors', 3))
+    added_parts = clf_builder.list_parts()
+
+    valid_parts = {
+                'classifier' : 'kNN',
+                'n_neighbors': 3, 
+                }
+    assert added_parts == valid_parts, "Add parts are not present in the classifier builder"
+
+    added_parts = clf_builder.list_parts()
+    n_parts = len(added_parts.keys())
+    assert n_parts == 2, "The amount of parts does not match the amount of add commands"
+    clf_builder.add(('silliness', 3))
+
+    added_parts = clf_builder.list_parts()
+    n_parts = len(added_parts.keys())
+    assert n_parts == 2, "An invalid part was added to the builder"
+
+    clf_builder.add(('algorithm', 'uniform'))
+    added_parts = clf_builder.list_parts()
+    n_parts = len(added_parts.keys())
+    print(added_parts)
+    assert n_parts == 3, "The amount of parts does not match the amount of add commands"
+
+def test_clf_builder_build():
+    clf_builder = Product1()
+    classifier = clf_builder.build()
+    assert type(classifier) == BuilderIsEmpty(), "An empty classifier builder should return EmptyBuilder"
+
+    clf_builder.add(('algorithm', 'uniform'))
+    classifier = clf_builder.build()
+    assert type(classifier) == BuilderMissingEssentialParts(), "A classifier without the essential parameters should return BuilderMissingEssentialParameter"
+    
+    clf_builder.add(('n_neighbors', 3))
+    classifier = clf_builder.build()
+    assert type(classifer) == Classifer, "A builder with all essential parameters should return a Classifier"
+
+    
+
+
+
+    
+    
 
 
 
