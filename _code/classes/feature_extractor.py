4cd7c9ced6ce709e57c4c47fb87ca0ccc3adeb95
M-wag <twelveandvoosted@gmail.com>
2023-02-25 18:19:46 +0100

    MVP for PreProcessor

--- _code/classes/feature_extractor.py
+++ _code/classes/feature_extractor.py
@@ -1,2 +1,19 @@
-class FeatureExtractor(object):
+class FeatureExtractor():
     pass
+
+class FeatureExtractorBuilder():
+    def __init__(self) -> None:
+        self.reset()
+
+    def reset(self) -> None:
+        self.parts = []
+
+    def list_parts(self):
+        return [part.__name__ for part in self.parts]
+
+    def add(self, filter_params: dict[str,any]) -> None:
+        pass
+
+
+    def build(self) -> PreProcesser:
+        return FeatureExtractor(self.parts)
--- _code/classes/preprocessor.py
+++ _code/classes/preprocessor.py
@@ -1,4 +1,17 @@
-from mne.filter import filter_data
+import mne
+import numpy as np
+from copy import deepcopy
+
+class PreProcesser():
+    def __init__(self, parts: list):
+        self.processes = parts
+        
+    def process_data(self, data: np.ndarray) -> np.ndarray:
+        for process in self.processes:
+            data = process(data)
+        return data
+    def list_processes():
+        return self.processes
 
 class PreProcessorBuilder():
     def __init__(self) -> None:
@@ -7,24 +20,23 @@ class PreProcessorBuilder():
     def reset(self) -> None:
         self.parts = []
 
+    def list_parts(self):
+        return [part.__name__ for part in self.parts]
+
     def add_filter(self, filter_params: dict[str,any]) -> None:
-        self.parts.append(filter_data(**filter_params))
+        def fir_filter(data: np.ndarray) -> np.ndarray:
+            filt_params = filter_params
+            filt_params['data'] = data
+            return mne.filter.filter_data(**filt_params)
+            
+        self.parts.append(fir_filter)
+
 
     def build(self) -> PreProcesser:
         return PreProcesser(self.parts)
         
 
 
-class PreProcesser():
-    def __init__(self, parts: list):
-        self.processes = parts
-        
-    def process_data(self, data: ndarray) -> ndarray:
-        for process in self.processes:
-            data = process(data)
-        return data
-    def list_processes():
-        return self.processes
             
 
 
