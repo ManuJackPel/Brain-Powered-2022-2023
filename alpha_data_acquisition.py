5c36240fc239919d54ce62085e6b73f2ce496ee3
scrie100 <36298464+scrie100@users.noreply.github.com>
2023-03-22 16:19:47 +0100

    good working plot, but we removed it as the sampling frequency turned out to be too low, now its around 249 Hz

--- alpha_data_acquisition.py
+++ alpha_data_acquisition.py
@@ -15,42 +15,58 @@ import scipy.signal
 from _code.classes.recorder import make_buffer, update_buffer
 from _code.classes.dataloader import MobiLab
 
-def datastream(vis_in, eye_in):
+def datastream(eye_in):
 #Receive incoming data from lsl server and send to other processes through pipe
 
-    eeg_stream = MobiLab()
-    header = ['time', 'CH1', 'CH2','CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8', 'CH9'] 
-    data_buffer = make_buffer(header, buffer_size=1024)
+    eeg_stream, start_time = MobiLab(), time.time()
 
-    start_time = time.time()
     while True:
-        start = time.time()
         sample, timestamp = eeg_stream.pull_sample()
         combined_array = np.array([timestamp] + sample)
-        data_buffer = update_buffer(data_buffer, combined_array)
 
-        if time.time() - start_time >= 0.5:
+        #Send 256 packets per second
+        if time.time() - start_time >= (0.00390625):
+            print(round(1/(time.time() - start_time), 1))
             start_time = time.time()
-            vis_in.send(data_buffer)
+
             eye_in.send(sample)
+            #vis_in.send(combined_array)
 
-def data_vis(pipe_end):
-# Plot voltage of channel one, received from pipe
+# def data_vis(pipe_end):
+# # Plot voltage of channel one, received from pipe
 
-    def animate(i):
-        data_buffer = pipe_end.recv()
-        time = data_buffer[:,0]
-        channels = data_buffer[:,[1,9]]
-        # print(time[-1])
+#     #data setup
+#     header = ['time', 'CH1', 'CH2','CH3', 'CH4', 'CH5', 'CH6', 'CH7', 'CH8', 'CH9'] 
+#     global data_buffer
+#     data_buffer, combined_array = make_buffer(header, buffer_size=1024), pipe_end.recv()
 
-        plt.cla()
-        plt.xlim([data_buffer[0,0], data_buffer[-1,0]])
-        plt.ylim([-3,3])
-        line = plt.plot(time, channels, '--')
-        return line,
+#     # First set up the figure, the axis, and the plot element we want to animate    
+#     fig = plt.figure()
+#     ax = plt.axes(xlim=[0, 1024], ylim=[-1,1])
+#     line, = ax.plot([], [], lw=2)
 
-    ani = FuncAnimation(plt.gcf(), animate, interval=12)    
-    plt.show()
+#     # initialization function: plot the background of each frame
+#     def init():
+#         line.set_data([],[])
+#         return line,
+
+#     # animation function.  This is called sequentially
+#     def animate(i):
+        
+#         #setup data
+#         global data_buffer
+#         combined_array = pipe_end.recv()        
+#         data_buffer = update_buffer(data_buffer, combined_array)
+#         time = range(1024)
+#         channels = data_buffer[:,1]
+        
+#         #push data to graph
+#         line.set_data(time, channels)
+#         return line,
+
+#     # call the animator.  blit=True means only re-draw the parts that have changed.
+#     ani = FuncAnimation(fig, animate, init_func=init, interval=(1/60), blit=False)    
+#     plt.show()
 
 
 def filesave(inputdata):
@@ -61,7 +77,7 @@ def filesave(inputdata):
     while True:
         
         #Read condition
-        with open('dev_code\psychopy\Psychopy\condition.txt') as f:
+        with open('dev_code\psychopy\condition.txt') as f:
             condition_file = f.readlines()
             condition = condition_file[0]
             participant = condition_file[1]
@@ -69,14 +85,14 @@ def filesave(inputdata):
         #Read sample from pipe and add condition as extra column
         sample = np.append(inputdata.recv(), condition)
 
-        print(np.shape(data))
-
         #Create data
         data = np.vstack((data, sample))
  
         #print data shape
-        print(np.shape(data))
-        print(condition)
+        #print(f"acquiring data from {participant}")
+        #print(np.shape(data))
+        #print(f"current state is {condition}")
+        
 
         if 'stop_threshold' in condition:
             print('saving the data')
@@ -86,19 +102,27 @@ def filesave(inputdata):
 
             np.savetxt(f'{participant}.csv', data, fmt="%s")
 
+            f = open('dev_code/psychopy/condition.txt', 'w')
+            f.writelines(['previous data has been saved\n', 'no current participant'])
+            f.close()
+
+            # Exit message and quit
+            print(f"data saved for {participant}, exiting now")
+            quit()
+
 
 
 if __name__ == "__main__":  # confirms that the code is under main function
     
     #declare pipeline, I declare, duplex = False is meant to make the pipe unidirectional
-    vis_out, vis_in = Pipe(duplex=False)
+    #vis_out, vis_in = Pipe(duplex=False)
     eye_out, eye_in = Pipe(duplex=False)
 
-    datastream_process = Process(target = datastream, args = (vis_in, eye_in))
+    datastream_process = Process(target = datastream, args = (eye_in,))
     datastream_process.start()
 
-    datavis_process = Process(target = data_vis, args = (vis_out,))
-    datavis_process.start()
+    #datavis_process = Process(target = data_vis, args = (vis_out,))
+    #datavis_process.start()
 
     filesave_process = Process(target = filesave, args = (eye_out,))
     filesave_process.start()
