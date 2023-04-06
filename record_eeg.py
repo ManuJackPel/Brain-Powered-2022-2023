31e2dff6f599676a50e580abf9717232436bf64c
M-wag <twelveandvoosted@gmail.com>
2023-04-06 14:18:01 +0200

    bugfix: change < to > in plotting while loop, bugfix: removed while true with open

--- record_eeg.py
+++ record_eeg.py
@@ -15,6 +15,20 @@ from _code.classes.recorder import make_buffer, update_buffer, Recorder
 from _code.classes.dataloader import DataStream
 
 
+def mute_stdout(func):
+    def wrapper(*args, **kwargs):
+        # Redirect stdout to a null device
+        devnull = open(os.devnull, 'w')
+        sys.stdout = devnull
+        # Call the function
+        result = func(*args, **kwargs)
+        # Restore stdout
+        sys.stdout = sys.__stdout__
+        return result
+    return wrapper
+
+
+# @mute_stdout
 def data_stream(stream_object, recorder_pipe_start, visualizer_pipe_start, condition_pipe_end):
     while True:
         # Get condition of task print('recover condition')
@@ -25,18 +39,19 @@ def data_stream(stream_object, recorder_pipe_start, visualizer_pipe_start, condi
             print('STOP TASK')
 
         # Pull sample
-        print('pull_sample')
+        # print('pull_sample')
         sample, timestamp = stream_object.pull_sample()
         # Combine into immutable array
-        print('combine arrays')
+        # print('combine arrays')
         combined_array = np.array([timestamp] + sample + [condition])
         combined_array.setflags(write=False)
         # Send data to recorder
-        print('send array to recorder')
+        # print('send array to recorder')
         recorder_pipe_start.send(combined_array)
         # Send data to visualizer 
-        print('send array to visualizer')
+        # print('send array to visualizer')
         visualizer_pipe_start.send(combined_array)
+        print(timestamp)
 
 def record_data_stream(recorder, recorder_pipe_end):
     # Start timer
@@ -45,7 +60,7 @@ def record_data_stream(recorder, recorder_pipe_end):
         # Get stream data
         combined_array = recorder_pipe_end.recv()
         # Append to recorder
-        recorder.append_data(combined_array)
+        # recorder.append_data(combined_array)
         # Save every n seconds
         if time.time() - start_time >= 0.5:
             start_time = time.time()
@@ -53,44 +68,49 @@ def record_data_stream(recorder, recorder_pipe_end):
             # recorder.save()
     
 def data_visualization(visualizer_pipe_end, header):
-    start_time = time.time()
-    # Generate initial data
-    x = np.linspace(0, 10, 100)
-    y = np.sin(x)
-
+    # Init buffer
+    buffer_size = 1024
+    plot_buffer = make_buffer(header, buffer_size)
     # Create figure and axis objects
     fig, ax = plt.subplots()
-
-    # Plot initial data
-    line, = ax.plot(x, y)
+    line, = ax.plot(np.zeros(buffer_size), np.zeros(buffer_size))
 
     # Continuously update the plot
+    start_time = time.time()
     while True:
-        visualizer_pipe_end.recv()
-        if time.time() - start_time < 0.5:
+        # Update buffer 
+        plot_buffer = update_buffer(plot_buffer, visualizer_pipe_end.recv())
+        if time.time() - start_time > 1:
+            # Reset timer
             start_time = time.time()
-            # Update the data
-            x += 0.3
-            y = np.sin(x)
+            # Get time and channel
+            time_ch = plot_buffer[:, 0]
+            volt_ch = plot_buffer[:, 1] # Get first and second electrode
+            condition_ch = plot_buffer[:, -1]
             # Update the plot
-            line.set_ydata(y)
+            line.set_xdata(time_ch)
+            line.set_ydata(volt_ch)
+            # print(time_ch[0], time_ch[-1])
             # Redraw the plot
             fig.canvas.draw()
         # PLT.pause somehow makes the plot show up
-        plt.pause(0.00001)
+        plt.pause(0.000000000000000000000000000000000000001)
             
 
 def get_alpha_task_condition(condition_pipe_start):
-    while True:
-        # TODO: check if file exist else make file
-        with open('dev_code/psychopy/condition.txt') as f:
+    # TODO: check if file exist else make file
+    with open('dev_code/psychopy/condition.txt') as f:
+        while True:
             condition_file = f.readlines()
+            # print('file: ', end='')
+            # print(condition_file)
             condition = condition_file[0]
             participant = condition_file[1] # Send data to recorder
             condition_pipe_start.send((condition, participant))
+            # Set file cursor back at start of file
+            f.seek(0)
 
 if __name__ == "__main__":  
-
     # stream_name = input('What is the name of the LSL stream: ')
     stream_name = 'mobilab'
     print('\nInitializing DataStream... ')
